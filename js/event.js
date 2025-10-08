c15c31665c893df23970ae65644d36a88e5646b9
sohanur24059 <sohanurrahman24059@gmail.com>
2025-10-08 21:30:51 +0600

    send this

--- js/event.js
+++ js/event.js
@@ -1,1 +1,12 @@
-conosle.log('This is seperate file')
+
+const purpleButton=document.getElementById('make-purple')
+            purpleButton.onclick=makePurple;
+
+            function makePurple(){
+                document.body.style.backgroundColor='purple';
+            }
+        // final 
+
+        document.getElementById('make-orange').addEventListener('click',function(){
+            document.body.style.backgroundColor='orange';
+        })
